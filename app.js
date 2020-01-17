c1ab24a45eaedf1c864d2f4834103f1e721d9eba
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-01-17 17:00:26 -0600

    okay I lied I am adding more

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 let makingMarks = document.querySelectorAll('.row div');
 let turns = 0;
+var draw = new Audio('draw.mp3');
+var win = new Audio('win.mp3');
+
+
 
 makingMarks.forEach(function (cell) {
     cell.addEventListener("click", playerMoves);
@@ -23,55 +27,65 @@ function checkWin() {
     //Top Horizontal Row Win
     if (makingMarks[0].textContent === "X" && makingMarks[1].textContent === "X" && makingMarks[2].textContent === "X" || makingMarks[0].textContent === "O" && makingMarks[1].textContent === "O" && makingMarks[2].textContent === "O") {
         if (turns % 2 === 0) {
-            alert("O Wins!");
+            setTimeout(function(){ alert("O Wins!"); }, 1);
             turns = 0;
+            win.play();
         } else {
-            alert("X Wins!");
+            setTimeout(function(){ alert("X Wins!"); }, 1);
             turns = 0;
+            win.play();
         }
     }
 
     //Middle Horizontal Line Win
     if (makingMarks[3].textContent === "X" && makingMarks[4].textContent === "X" && makingMarks[5].textContent === "X" || makingMarks[3].textContent === "O" && makingMarks[4].textContent === "O" && makingMarks[5].textContent === "O") {
         if (turns % 2 === 0) {
-            alert("O Wins!");
+            setTimeout(function(){ alert("O Wins!"); }, 1);
             turns = 0;
+            win.play();
         } else {
-            alert("X Wins!");
+            setTimeout(function(){ alert("X Wins!"); }, 1);
             turns = 0;
+            win.play();
         }
     }
 
     //Bottom Horizontal Row Win
     if (makingMarks[6].textContent === "X" && makingMarks[7].textContent === "X" && makingMarks[8].textContent === "X" || makingMarks[6].textContent === "O" && makingMarks[7].textContent === "O" && makingMarks[8].textContent === "O") {
         if (turns % 2 === 0) {
-            alert("O Wins!");
+            setTimeout(function(){ alert("O Wins!"); }, 1);
             turns = 0;
+            win.play();
         } else {
-            alert("X Wins!");
+            setTimeout(function(){ alert("X Wins!"); }, 1);
             turns = 0;
+            win.play();
         }
     }
 
     //First Vertical Column Win
     if (makingMarks[0].textContent === "X" && makingMarks[3].textContent === "X" && makingMarks[6].textContent === "X" || makingMarks[0].textContent === "O" && makingMarks[3].textContent === "O" && makingMarks[6].textContent === "O") {
         if (turns % 2 === 0) {
-            alert("O Wins!");
+            setTimeout(function(){ alert("O Wins!"); }, 1);
             turns = 0;
+            win.play();
         } else {
-            alert("X Wins!");
+            setTimeout(function(){ alert("X Wins!"); }, 1);
             turns = 0;
+            win.play();
         }
     }
 
     //Second Vertical Column Win
     if (makingMarks[1].textContent === "X" && makingMarks[4].textContent === "X" && makingMarks[7].textContent === "X" || makingMarks[1].textContent === "O" && makingMarks[4].textContent === "O" && makingMarks[7].textContent === "O") {
         if (turns % 2 === 0) {
-            alert("O Wins!");
+            setTimeout(function(){ alert("O Wins!"); }, 1);
             turns = 0;
+            win.play();
         } else {
-            alert("X Wins!");
+            setTimeout(function(){ alert("X Wins!"); }, 1);
             turns =0;
+            win.play();
         }
     }
 
@@ -79,38 +93,45 @@ function checkWin() {
     //Third Vertical Column Win
     if (makingMarks[2].textContent === "X" && makingMarks[5].textContent === "X" && makingMarks[8].textContent === "X" || makingMarks[2].textContent === "O" && makingMarks[5].textContent === "O" && makingMarks[8].textContent === "O") {
         if (turns % 2 === 0) {
-            alert("O Wins!");
+            setTimeout(function(){ alert("O Wins!"); }, 1);
             turns = 0;
+            win.play();
         } else {
-            alert("X Wins!");
+            setTimeout(function(){ alert("X Wins!"); }, 1);
             turns = 0;
+            win.play();
         }
     }
 
     //Top Left Corner to Bottom Right Corner Diagonal Win
     if (makingMarks[0].textContent === "X" && makingMarks[4].textContent === "X" && makingMarks[8].textContent === "X" || makingMarks[0].textContent === "O" && makingMarks[4].textContent === "O" && makingMarks[8].textContent === "O") {
         if (turns % 2 === 0) {
-            alert("O Wins!");
+            setTimeout(function(){ alert("O Wins!"); }, 1);
             turns = 0;
+            win.play();
         } else {
-            alert("X Wins!");
+            setTimeout(function(){ alert("X Wins!"); }, 1);
             turns = 0;
+            win.play();
         }
     }
 
     //Top Right Corner to Bottom Left Corner Diagonal Win
     if (makingMarks[2].textContent === "X" && makingMarks[4].textContent === "X" && makingMarks[6].textContent === "X" || makingMarks[2].textContent === "O" && makingMarks[4].textContent === "O" && makingMarks[6].textContent === "O") {
         if (turns % 2 === 0) {
-            alert("O Wins!");
+            setTimeout(function(){ alert("O Wins!"); }, 1);
             turns = 0;
+            win.play();
         } else {
-            alert("X Wins!");
+            setTimeout(function(){ alert("X Wins!"); }, 1);
             turns = 0;
+            win.play();
         }
     }
 
     if (turns === 9) {
-        alert("Everybody Loses!")
+        draw.play();
+        setTimeout(function(){ alert("This is not the way!"); }, 1);
     }
 }
 
